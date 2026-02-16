ecb27fdd9660ba0b7ae3780cb1af484294366c6d
FALLEN-01 <jeffinbasil484@gmail.com>
2026-02-16 12:06:14 +0530

    added empty entertainment page

---
+++ app/entertainment/page.tsx
@@ -0,0 +1,9 @@
+export default function EntertainmentPage() {
+    return (
+        <div className="min-h-screen bg-[#1A0000] flex items-center justify-center">
+            <h1 className="text-4xl md:text-6xl font-bold text-[#FFD120]">
+                Entertainment Page
+            </h1>
+        </div>
+    );
+}
